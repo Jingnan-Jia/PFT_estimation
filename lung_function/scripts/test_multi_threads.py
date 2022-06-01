8e403e2bb1baa78b9014f104753fb45f4e17ef7e
Jingnan-Jia <jiajingnan2222@gmail.com>
2022-06-01 16:03:25 +0200

    jobid is 323864

--- lung_function/scripts/test_multi_threads.py
+++ lung_function/scripts/test_multi_threads.py
@@ -25,32 +25,65 @@ import random
 args = get_args()
 global_lock = threading.Lock()
 
-def sub_thread():
+def sub_thread1():
     for i in range(500):  ## 100 seconds
-        # with global_lock:
-        tmp2 = random.random()
-        log_metric('Accuracy2', tmp2, step=i)
-        print(f'accuracy2: {tmp2}')
-        # time.sleep(0.5)
+        with global_lock:
+            tmp1 = random.random()
+            log_metric('Accuracy1', tmp1, step=i)
+            print(f'accuracy1: {tmp1}')
+        time.sleep(tmp1)
+
+def sub_thread2():
+    for i in range(500):  ## 100 seconds
+        with global_lock:
+            tmp2 = random.random()
+            log_metric('Accuracy2', tmp2, step=i)
+            print(f'accuracy2: {tmp2}')
+        time.sleep(tmp2)
+
+
+def sub_thread3():
+    for i in range(500):  ## 100 seconds
+        with global_lock:
+            tmp3 = random.random()
+            log_metric('Accuracy3', tmp3, step=i)
+            print(f'accuracy3: {tmp3}')
+        time.sleep(tmp3)
+
+def sub_thread4():
+    for i in range(500):  ## 100 seconds
+        with global_lock:
+            tmp4 = random.random()
+            log_metric('Accuracy4', tmp4, step=i)
+            print(f'accuracy4: {tmp4}')
+        time.sleep(tmp4)
 
 if __name__ == "__main__":
     mlflow.set_tracking_uri("http://nodelogin02:5000")
     with mlflow.start_run():
-        p1 = threading.Thread(target=sub_thread)
-        p1.start()
+        log_params(vars(args))
 
-        p2 = threading.Thread(target=record_cgpu_info, args=(args.outfile, global_lock))
+        p1 = threading.Thread(target=sub_thread1)
+        p2 = threading.Thread(target=sub_thread2)
+        p3 = threading.Thread(target=sub_thread3)
+        p4 = threading.Thread(target=sub_thread3)
+
+        p1.start()
         p2.start()
+        p3.start()
+        p4.start()
 
         for i in range(500): ## 100 seconds
-            # with global_lock:
-            tmp1 = random.random()
-            log_metric('Accuracy1', tmp1, step=i)
-            print(f'accuracy1: {tmp1}')
-            time.sleep(0.5)
+            with global_lock:
+                tmp0 = random.random()
+                log_metric('Accuracy0', tmp0, step=i)
+                print(f'accuracy0: {tmp0}')
+            time.sleep(tmp0)
 
 
         p1.join()
         p2.join()
+        p3.join()
+        p4.join()
 
     print("Finished!")
