aa6e7bed6ef420a8511d7e9e87bae62349bf5d3e
Jingnan-Jia <jiajingnan2222@gmail.com>
2022-12-25 12:47:12 +0100

    jobid is 931047

--- lung_function/scripts/run.py
+++ lung_function/scripts/run.py
@@ -221,8 +221,8 @@ class Run:
                 points = provider.random_point_dropout(points)
                 # points[:, :, 0:3] = provider.random_scale_point_cloud(
                 #     points[:, :, 0:3])
-                # points[:, :, 0:3] = provider.shift_point_cloud(
-                #     points[:, :, 0:3])
+                points[:, :, 0:3] = provider.shift_point_cloud(
+                    points[:, :, 0:3])
                 points = torch.Tensor(points)
                 points = points.transpose(2, 1)
                 data[key] = points
