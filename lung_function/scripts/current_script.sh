9c8501e1cd7b6f207146e3917f8c3a980bc90dac
Jingnan-Jia <jiajingnan2222@gmail.com>
2023-04-15 23:31:55 +0200

    jobid is 1383860

--- lung_function/scripts/current_script.sh
+++ lung_function/scripts/current_script.sh
@@ -72,4 +72,4 @@ conda activate py38
 
 # shellcheck disable=SC2046
 # idx=0; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u run.py 2>${slurm_dir}/slurm-${job_id}_${idx}_err.txt 1>${slurm_dir}/slurm-${job_id}_${idx}_out.txt --outfile=${slurm_dir}/slurm-${job_id}_$idx --hostname="$(hostname)" --jobid=${job_id} --remark="balanced sampler, lr=0.001"
-idx=0; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u run.py 2>${slurm_dir}/slurm-${job_id}_${idx}_err.txt 1>${slurm_dir}/slurm-${job_id}_${idx}_out.txt --outfile=${slurm_dir}/slurm-${job_id}_$idx --hostname="$(hostname)" --jobid=${job_id} --net='vgg11_3d' --batch_size=5 --input_mode="ct_masked_by_torso" --remark="%pred, verify more networks"
+idx=0; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u run.py 2>${slurm_dir}/slurm-${job_id}_${idx}_err.txt 1>${slurm_dir}/slurm-${job_id}_${idx}_out.txt --outfile=${slurm_dir}/slurm-${job_id}_$idx --hostname="$(hostname)" --jobid=${job_id} --net='vgg11_3d' --batch_size=5 --input_mode="ct_masked_by_torso" --target='DLCOcPP' --remark="single output, %pred, verify more networks"
